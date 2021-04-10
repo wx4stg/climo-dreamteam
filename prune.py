c3a74aadb409994bb85b4dffa499fb64ceef19d8
Sam Gardner <stgardner4@tamu.edu>
2021-04-10 02:52:14 -0500

    remove the buoy stations

---
+++ prune.py
@@ -0,0 +1,9 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+filename = "asos-2021-full"
+badStations = ["CRH", "GVX", "BQX", "BBF", "MZG", "MIU", "OPM", "GYF", "25T", "VAF", "GUL", "EMK", "HQI"]
+pandasData = pd.read_csv(str(filename+".csv"), keep_default_na=False)
+for badStation in badStations:
+    pandasData = pandasData[pandasData.station != badStation]
+pandasData.to_csv(str(filename+".csv"), index=False)
