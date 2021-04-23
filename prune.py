5198a8efdd70e043fc44f7517222e45f3c633939
Sam Gardner <stgardner4@tamu.edu>
2021-04-22 20:22:42 -0500

    remove more buoys

--- prune.py
+++ prune.py
@@ -2,7 +2,7 @@ import pandas as pd
 pd.options.mode.chained_assignment = None
 
 filename = "asos-2021-full"
-badStations = ["CRH", "GVX", "BQX", "BBF", "MZG", "MIU", "OPM", "GYF", "25T", "VAF", "GUL", "EMK", "HQI"]
+badStations = ["CRH", "GVX", "BQX", "BBF", "MZG", "MIU", "OPM", "GYF", "25T", "VAF", "GUL", "EMK", "HQI", "HHV", "VKY"]
 pandasData = pd.read_csv(str(filename+".csv"), keep_default_na=False)
 for badStation in badStations:
     pandasData = pandasData[pandasData.station != badStation]
